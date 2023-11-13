eaf917c7cc4b49b3532bccc4e618b87b422c2ef7
Fernando Garcia <113075299+GarciaFernando1@users.noreply.github.com>
2023-11-13 01:46:37 -0300

    Add files via upload

---
+++ client/js/products.js
@@ -0,0 +1,128 @@
+const productos = [
+    {
+        id: 1,
+        productName: 'Campera Impermeable',
+        price: 8900,
+        quanty : 1,
+        img: './img/abrigos/01.jpg',
+    }
+    ,
+    {
+        id: 2,
+        productName: 'Campera Impermeable',
+        price: 8400,
+        quanty : 1,
+        img: './img/abrigos/02.jpg',
+    },
+    {
+        id: 3,
+        productName: 'Campera Polar',
+        price: 11300,
+        quanty : 1,
+        img: './img/abrigos/03.jpg',
+    },    {
+        id: 4,
+        productName: 'Campera Running',
+        price: 7800,
+        quanty : 1,
+        img: './img/abrigos/04.jpg',
+    }
+    ,
+    {
+        id: 5,
+        productName: 'Campera Algodon',
+        price: 7800,
+        quanty : 1,
+        img: './img/abrigos/05.jpg',
+    },
+    {
+        id: 6,
+        productName: 'Remera lúpulo Amarilla',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/01.jpg',
+    },    {
+        id: 7,
+        productName: 'Remera lúpulo Azul',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/02.jpg',
+    }
+    ,
+    {
+        id: 8,
+        productName: 'Remera Althon Azul',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/03.jpg',
+    },
+    {
+        id: 9,
+        productName: 'Remera Althon Beige',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/04.jpg',
+    },    {
+        id: 10,
+        productName: 'Remera Althon Gris',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/05.jpg',
+    }
+    ,
+    {
+        id: 11,
+        productName: 'Remera Champion',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/06.jpg',
+    },
+    {
+        id: 12,
+        productName: 'Remera Basica Verde',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/07.jpg',
+    },    {
+        id: 13,
+        productName: 'Remera Basica Celeste',
+        price: 5300,
+        quanty : 1,
+        img: './img/camisetas/08.jpg',
+    }
+    ,
+    {
+        id: 14,
+        productName: 'Pantalon Babucha Negro',
+        price: 15600,
+        quanty : 1,
+        img: './img/pantalones/01.jpg',
+    },
+    {
+        id: 15,
+        productName: 'Pantalon Cargo Gris Oscuro',
+        price: 15600,
+        quanty : 1,
+        img: './img/pantalones/02.jpg',
+    },    {
+        id: 16,
+        productName: 'Pantalon Cargo Verde Militar',
+        price: 15600,
+        quanty : 1,
+        img: './img/pantalones/03.jpg',
+    }
+    ,
+    {
+        id: 17,
+        productName: 'Pantalon Cargo Beige',
+        price: 15600,
+        quanty : 1,
+        img: './img/pantalones/04.jpg',
+    },
+    {
+        id: 18,
+        productName: 'Pantalon Babucha Celeste',
+        price: 15600,
+        quanty : 1,
+        img: './img/pantalones/05.jpg',
+    }];
